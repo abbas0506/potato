27cb4ae0487e8607a2696bf4d31f3461b0f8403b
abbas <abbas.sscs@gmail.com>
2021-12-07 19:38:49 +0500

    071221

---
+++ database/migrations/2021_12_06_171807_create_wastes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWastesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wastes', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('purchase_id');
+            $table->unsignedInteger('numofbori');
+            $table->unsignedInteger('numoftora');
+            $table->string('note', 200);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wastes');
+    }
+}
