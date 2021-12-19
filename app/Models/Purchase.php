3861a839cf1c3689aeac648db7a1aeb79e6b5c6c
abbas <abbas.sscs@gmail.com>
2021-12-19 12:33:20 +0500

    191221

--- app/Models/Purchase.php
+++ app/Models/Purchase.php
@@ -29,15 +29,7 @@ class Purchase extends Model
     {
         return $this->belongsTo(Deal::class, 'deal_id');
     }
-    // public function client()
-    // {
-    //     return $this->deal()->client();
-    // }
 
-    // public function product()
-    // {
-    //     return $this->deal()->product();
-    // }
     public function storages()
     {
         return $this->hasMany(Storage::class, 'purchase_id');
@@ -46,15 +38,50 @@ class Purchase extends Model
     {
         return $this->hasMany(Sale::class, 'purchase_id');
     }
+    public function stores()
+    {
+        $store_ids = $this->storages->unique()->pluck('store_id')->toArray();
+        return Store::whereIn('id', $store_ids)->get();
+    }
+
     public function stored()
     {
         return $this->storages()->sum('numofbori') . "-" . $this->storages()->sum('numoftora');
     }
+
+    public function numofbori_stored()
+    {
+        return $this->storages()->sum('numofbori');
+    }
+    public function numoftora_stored()
+    {
+        return $this->storages()->sum('numoftora');
+    }
+
+    public function numofbori_sold()
+    {
+        return $this->sales()->sum('numofbori');
+    }
+    public function numoftora_sold()
+    {
+        return $this->sales()->sum('numoftora');
+    }
+    public function numofbori_left()
+    {
+        return $this->numofbori - $this->numofbori_sold() - $this->numofbori_stored();
+    }
+    public function numoftora_left()
+    {
+        return $this->numoftora - $this->numoftora_sold() - $this->numoftora_stored();
+    }
+
+
+
     public function sold()
     {
         return $this->sales()->sum('numofbori') . "-" . $this->sales()->sum('numoftora');
     }
-    public function stock()
+    public function left()
     {
         $numofbori_purchased = $this->numofbori;
         $numoftora_purchased = $this->numoftora;
